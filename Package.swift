c919f896c68cd1595742f11c38fe43a99232b086
tsanto <tiagosanto.work@gmail.com>
2020-10-11 18:38:11 +0100

    Fix dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "AlamofireXMLRPC",
+    platforms: [.macOS(.v10_12),
+                .iOS(.v10),
+                .tvOS(.v10),
+                .watchOS(.v3)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,15 +16,15 @@ let package = Package(
             targets: ["AlamofireXMLRPC"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.2.2"),
-        .package(url: "https://github.com/tadija/AEXML.git", from: "4.6.0")
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "4.9.0"),
+        .package(url: "https://github.com/tsanto/AEXML", from: "5.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AlamofireXMLRPC",
-            dependencies: []),
+            dependencies: ["Alamofire", "AEXML"]),
         .testTarget(
             name: "AlamofireXMLRPCTests",
             dependencies: ["AlamofireXMLRPC"]),
