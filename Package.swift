92edb44481d196cd2237e54862d4b9a861bf2865
tsanto <tiagosanto.work@gmail.com>
2020-10-11 18:28:28 +0100

    Add products

--- Package.swift
+++ Package.swift
@@ -5,9 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "AlamofireXMLRPC",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AlamofireXMLRPC",
+            targets: ["AlamofireXMLRPC"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.2.2"),
         .package(url: "https://github.com/tadija/AEXML.git", from: "4.6.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "AlamofireXMLRPC",
+            dependencies: []),
+        .testTarget(
+            name: "AlamofireXMLRPCTests",
+            dependencies: ["AlamofireXMLRPC"]),
     ]
 )
 
