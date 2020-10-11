e201becb3c8f270b58359eab558f57560be3fd1a
tsanto <tiagosanto.work@gmail.com>
2020-10-11 18:23:55 +0100

    Update swift version and dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:3.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "AlamofireXMLRPC",
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 4),
-        .Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4)
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.2.2"),
+        .package(url: "https://github.com/tadija/AEXML.git", from: "4.6.0")
     ]
 )
 
